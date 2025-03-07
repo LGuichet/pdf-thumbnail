a9016e37c6a588c399d2179881d3e85722a97b64
Louis Guichet <louis.guichet@avec.fr>
2025-03-07 18:12:34 +0100

    feature: save pdf on file systm

--- src/service.ts
+++ src/service.ts
@@ -1,12 +1,13 @@
 import {CompletedGenerationJob} from "./models/completed_generation_job.model";
 import {PdfRequest} from "./models/pdf_request.model";
+import { saveToFileSystem } from "./pdf_storage";
 
 export async function savePdfProcessingRequest(
   pdf: Buffer
 ): Promise<PdfRequest> {
   const request = await PdfRequest.create();
   const fileName = request.id + ".pdf";
-  // save buffer to fs here
+  saveToFileSystem(pdf, fileName);
   return request;
 }
 
